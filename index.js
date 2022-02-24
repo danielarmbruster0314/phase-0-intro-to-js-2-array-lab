4460660897d39b61c42fb848586ffa9cffefffa1
Daniel Armbruster <danielgundam00@gmail.com>
2022-02-24 16:52:29 -0700

    commit tree b4bcfcd0351ae47f31f46f39e5a36af421b7ad8d

--- index.js
+++ index.js
@@ -2,33 +2,33 @@
 const cats = ["Milo", "Otis", "Garfield"];
 
 function destructivelyAppendCat(name){
-    return cats.push("Ralph");
+        return cats.push(name);
 }
 
 function destructivelyPrependCat(name){
-    return cats.unshift("Bob");
+    return cats.unshift(name);
 }
 
-function destructivelyRemoveLastCat(name){
+function destructivelyRemoveLastCat(){
     return cats.pop();
 }
 
-function destructivelyRemoveFirstCat(name){
+function destructivelyRemoveFirstCat(){
     return cats.shift();
 }
 
 function appendCat(name){
-    return [] = [...cats, "Broom"];
+    return [...cats,name];
 }
 
 function prependCat(name){
-    return [] = ["Arnold",...cats];
-}
-
-function removeLastCat(){
-    return cats.slice(0,-1);
+    return [name,...cats];
 }
 
 function removeFirstCat(){
     return cats.slice(1);
+}
+
+function removeLastCat(){
+    return cats.slice(0,cats.length-1);
 }
